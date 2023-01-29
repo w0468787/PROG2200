b4b9e01991706fab0bc71aa6dec7a2a5c14815dc
Drew Villard <w0468787@campus.nscc.ca>
2023-01-28 23:59:56 -0400

    gitIgnore trial and challenge 2 complete

---
+++ Lab1/Module1/Section1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            var age = Console.ReadLine();
+            Console.WriteLine("What month were you born in?");
+            var bMonth = Console.ReadLine();
+            Console.WriteLine("What is your favorite colour");
+            var colour = Console.ReadLine();
+            Console.WriteLine("whats the weather like?");
+            var weather = Console.ReadLine();
+           
+
+            Console.WriteLine("Your name is: "+name);
+            Console.WriteLine("You are "+age+" years old");
+            Console.WriteLine("You were born in: " + bMonth);
+            Console.WriteLine("Your favorite colour is: " + colour);
+            Console.WriteLine("The weather is "+weather);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Lab1/Module1/Section2/PasswordChallenge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PasscodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var secret = "secret";
+            Console.WriteLine("Enter the secret code");
+            var code = Console.ReadLine();
+            if (code.Equals(secret))
+            {
+                Console.WriteLine("user authenticated do you want to change the password Y/N?");
+                var choice = Console.ReadLine();
+                if (choice.Equals("Y"))
+                {
+                    secret = Console.ReadLine();
+                    Console.WriteLine("Password Changed to " + secret);
+                }
+                else { Console.WriteLine("OK"); }
+            }
+            else {
+                Console.WriteLine("Incorrect");
+            }
+        }
+    }
+}
