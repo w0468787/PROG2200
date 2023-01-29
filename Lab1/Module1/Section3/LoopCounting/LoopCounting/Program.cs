ce6c6cbbeac66bae05150b85424aacbd574b3111
Drew Villard <w0468787@campus.nscc.ca>
2023-01-29 01:14:06 -0400

    Challenge 3 complete

---
+++ Lab1/Module1/Section3/LoopCounting/LoopCounting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LoopCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int m = 10;
+            int y = 1;
+            while (x <= 5)
+            {
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    if (x % 2 == 0)
+                    {
+                        m -= y;
+                        Console.WriteLine(m);
+                    }
+                    else { 
+                    Console.WriteLine(i);
+                    }
+                   
+                }
+                x++;
+                m = 10;
+            }
+        }
+    }
+}
